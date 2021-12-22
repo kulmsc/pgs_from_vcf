f6fde9de523b8fb14d5eea661890ce6fb8e709cc
Scott Kulm <kulmsc@nice.pbtech>
2021-12-22 07:32:35 -0500

    try

---
+++ prep_vcf/prep_vcf.sh
@@ -0,0 +1,29 @@
+#filename=$1
+ls ../raw_vcf/ | while read filename;do
+
+  prefix=`echo $filename | cut -f1 -d'.'`
+  if [ ! -e ${prefix}.lifted.vcf.gz ];then
+
+    #liftover to grch37
+    zcat ../raw_vcf/$filename | fgrep -v "*" | gzip > ready_for_lift.vcf.gz
+    java -jar ~/Programs/picard/build/libs/picard.jar LiftoverVcf I=ready_for_lift.vcf.gz O=temp.lifted.vcf  CHAIN=/home/kulmsc/athena/refs/for_picard/hg38ToHg19.over.chain REJECT=rejected_variants.vcf R=/home/kulmsc/athena/refs/for_picard/hg19.fa
+
+    cat temp.lifted.vcf | awk '{gsub(/^chr/,""); print}' > new.vcf
+    rm temp.lifted.vcf
+
+    bgzip new.vcf
+
+    bcftools index new.vcf.gz
+
+    bcftools annotate -a /home/kulmsc/athena/refs/Homo_sapiens_assembly19.dbsnp138.vcf.gz -c ID -o ${prefix}.lifted.vcf new.vcf.gz
+
+    gzip ${prefix}.lifted.vcf
+    rm new.vcf.gz
+    rm new.vcf.gz.csi
+    rm temp.lifted.vcf.idx
+    rm rejected_variants.vcf
+    rm ready_for_lift.vcf.gz
+
+    ./get_bad_rsids.sh ${prefix}
+  fi
+done

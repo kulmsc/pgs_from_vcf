82ec778b6f79532a364df364167faa1c753a5d8f
Scott Kulm <kulmsc@nice.pbtech>
2022-02-22 15:02:44 -0500

    test

---
+++ raw_catalog_sets/pos_refiner_ss.R
@@ -0,0 +1,136 @@
+library(vroom)
+library(stringr)
+library(bigsnpr)
+
+options(warn=2)
+
+#Read in the UKBB summary statistics
+impute <- read.table("~/athena/doc_score/raw_ss/common_files/impute_rsids", header = F, stringsAsFactors = F)
+colnames(impute) <- c("LOC", "SNP", "POS", "A1", "A2", "MAF", "AX", "INFO")
+
+#Make sure the UKBB summary statistics have alleles that match ACGT and are not ambigous
+impute <- impute[nchar(impute$A1) == 1 & nchar(impute$A2) == 1,]
+impute <- impute[impute$A1 %in% c("A", "C", "G", "T") & impute$A2 %in% c("A", "C", "G", "T"),]
+impute <- impute[!((impute$A1 == "A" & impute$A2 == "T") |
+             (impute$A1 == "T" & impute$A2 == "A") |
+             (impute$A1 == "G" & impute$A2 == "C") |
+             (impute$A1 == "C" & impute$A2 == "G")),]
+
+#Read in other UKBB data that comes with chromosomes so I can give a chromosome to each SNP
+impute$CHR <- "X"
+for(i in 1:22){
+  temp <- readRDS(paste0("~/athena/ukbiobank/qc/imputed/chr", i, ".RDS"))
+  impute$CHR[impute$SNP %in% temp[,2]] <- i
+}
+impute <- impute[impute$CHR %in% 1:22,]
+impute$SUPERPOS <- paste0(impute$CHR, "_", impute$POS)
+
+#Now iterating through each of the score summary statistics
+#Now iterating through each of the score summary statistics
+x <- unique(grep("PGS", unlist(strsplit(list.files("../pgs_sets/"), ".", fixed=T)), value=T))
+y <- grep("PGS", unlist(strsplit(list.files(), ".", fixed=T)), value=T)
+todo <- paste0(y[!(y %in% x)], ".txt.gz")
+
+
+###
+print("iterating through files")
+for(filename in todo){
+  print(filename)
+  
+  #get the reference genome of the score file to see if we can match the positions only
+  system(paste0("zcat ", filename, " | fgrep build | cut -f2 -d'=' > temp"))
+  build_name <- read.table("temp", stringsAsFactors=F)
+  system("rm temp")
+
+  if(build_name[,1] == "hg19" | build_name[,1] == "GRCh37"){
+    print("pass: reference genome")
+
+    #read the summary statistic in
+    ss <- read.table(paste0(filename), stringsAsFactors=F, header=T, sep='\t')
+    print(dim(ss))
+
+    if("effect_allele" %in% colnames(ss)){
+      print("pass: effect allele")
+
+      if(!("reference_allele" %in% colnames(ss))){
+        print("fail: reference allele - going to work around")
+        ss <- ss[!is.na(ss$rsID),]
+        ss$rsID <- tolower(ss$rsID)
+        ss <- ss[ss$rsID %in% impute$SNP,]
+        sub_impute <- impute[impute$SNP %in% ss$rsID,]
+        ss <- ss[order(ss$rsID)[rank(sub_impute$SNP)],]
+
+        comp_allele <- rep("X", nrow(ss))
+        comp_allele[ss$effect_allele == "A"] <- "T"
+        comp_allele[ss$effect_allele == "T"] <- "A"
+        comp_allele[ss$effect_allele == "C"] <- "G"
+        comp_allele[ss$effect_allele == "G"] <- "C"
+
+        ref_allele <- rep("X", nrow(ss))
+        ref_allele[ss$effect_allele == sub_impute$A1] <- sub_impute$A2[ss$effect_allele == sub_impute$A1]
+        ref_allele[ss$effect_allele == sub_impute$A2] <- sub_impute$A1[ss$effect_allele == sub_impute$A2]
+        ss$effect_allele[ref_allele == "X"] <- comp_allele[ref_allele == "X"]
+        ref_allele[ss$effect_allele == sub_impute$A1 & ref_allele == "X"] <- sub_impute$A2[ss$effect_allele == sub_impute$A1 & ref_allele == "X"]
+        ref_allele[ss$effect_allele == sub_impute$A2 & ref_allele == "X"] <- sub_impute$A1[ss$effect_allele == sub_impute$A2 & ref_allele == "X"]
+
+        ss$reference_allele <- ref_allele
+        sub_impute <- sub_impute[ss$reference_allele != "X",]
+        ss <- ss[ss$reference_allele != "X",]
+      }
+
+      #limit SNPs to those with ACGT
+      ss$effect_allele <- toupper(ss$effect_allele)
+      ss$reference_allele <- toupper(ss$reference_allele)
+      ss <- ss[nchar(ss$effect_allele) == 1 & nchar(ss$reference_allele) == 1,]
+      ss <- ss[ss$effect_allele %in% c("A", "C", "G", "T") & ss$reference_allele %in% c("A", "C", "G", "T"),]
+
+      #remove ambigous SNPs
+      ss <- ss[!((ss$effect_allele == "A" & ss$reference_allele == "T") |
+               (ss$effect_allele == "T" & ss$reference_allele == "A") |
+               (ss$effect_allele == "G" & ss$reference_allele == "C") |
+               (ss$effect_allele == "C" & ss$reference_allele == "G")),]
+
+      #if rsID in the ss use it to match and sort to the UKBB
+      #if not then use the chromosome and position (I already checked that all scores' ref genomes match UKBB)
+      if("rsID" %in% colnames(ss)){
+        print("pass: rsid present") 
+        ss <- ss[!is.na(ss$rsID),]
+        ss$rsID <- tolower(ss$rsID)
+        ss <- ss[ss$rsID %in% impute$SNP,]
+        sub_impute <- impute[impute$SNP %in% ss$rsID,]
+        ss <- ss[order(ss$rsID)[rank(sub_impute$SNP)],]
+      } else {
+        print("fail: rsid not present - going to workaround")
+        ss$SUPERPOS <- paste0(ss$chr_name, "_", ss$chr_position)
+        ss <- ss[ss$SUPERPOS %in% impute$SUPERPOS,]
+        sub_impute <- impute[impute$SUPERPOS %in% ss$SUPERPOS,]
+        ss <- ss[order(ss$SUPERPOS)[rank(sub_impute$SUPERPOS)],]
+        ss$rsID <- sub_impute$SNP    
+      }
+      print(dim(ss))
+
+      #Change the column names and then use the snp_match function from bigsnpr to flip alleles
+      colnames(sub_impute) <- c("loc", "rsid", "pos", "a0", "a1", "maf", "ax", "info", "chr", "superpos")
+      colnames(ss)[colnames(ss) == "effect_allele"] <- "a0"
+      colnames(ss)[colnames(ss) == "reference_allele"] <- "a1"
+      colnames(ss)[colnames(ss) == "rsID"] <- "rsid"
+      colnames(ss)[colnames(ss) == "effect_weight"] <- "beta"
+      ss$pos <- sub_impute$pos
+      ss$chr <- sub_impute$chr
+      ss <- snp_match(ss, sub_impute)
+      print(dim(ss))
+
+      #write out the refined summary statistic
+      print("writing out")
+      out_ss <- data.frame(chr = ss$chr, rsid = ss$rsid.ss, A1 = ss$a0, beta = ss$beta)
+      new_name <- strsplit(filename, ".", fixed = T)[[1]][1]
+      for(ii in 1:22){
+        if(ii %in% out_ss[,1]){
+        write.table(out_ss[out_ss[,1] == ii,], paste0("../pgs_sets/", new_name, ".", ii, ".ss"), col.names = T, row.names = F, sep = '\t', quote=F)
+        }
+      }
+     print("done\n")
+
+   }
+  }
+}
---
+++ raw_catalog_sets/refiner_ss.R
@@ -0,0 +1,96 @@
+library(vroom)
+library(stringr)
+library(bigsnpr)
+
+options(warn=2)
+
+#Read in the UKBB summary statistics
+impute <- read.table("~/athena/doc_score/raw_ss/common_files/impute_rsids", header = F, stringsAsFactors = F)
+colnames(impute) <- c("LOC", "SNP", "POS", "A1", "A2", "MAF", "AX", "INFO")
+
+#Make sure the UKBB summary statistics have alleles that match ACGT and are not ambigous
+impute <- impute[nchar(impute$A1) == 1 & nchar(impute$A2) == 1,]
+impute <- impute[impute$A1 %in% c("A", "C", "G", "T") & impute$A2 %in% c("A", "C", "G", "T"),]
+impute <- impute[!((impute$A1 == "A" & impute$A2 == "T") |
+             (impute$A1 == "T" & impute$A2 == "A") |
+             (impute$A1 == "G" & impute$A2 == "C") |
+             (impute$A1 == "C" & impute$A2 == "G")),]
+
+#Read in other UKBB data that comes with chromosomes so I can give a chromosome to each SNP
+impute$CHR <- "X"
+for(i in 1:22){
+  temp <- readRDS(paste0("~/athena/ukbiobank/qc/imputed/chr", i, ".RDS"))
+  impute$CHR[impute$SNP %in% temp[,2]] <- i
+}
+impute <- impute[impute$CHR %in% 1:22,]
+impute$SUPERPOS <- paste0(impute$CHR, "_", impute$POS)
+
+#Now iterating through each of the score summary statistics
+x <- unique(grep("PGS", unlist(strsplit(list.files("../pgs_sets/"), ".", fixed=T)), value=T))
+y <- grep("PGS", unlist(strsplit(list.files(), ".", fixed=T)), value=T)
+todo <- paste0(y[!(y %in% x)], ".txt.gz")
+
+###
+print("starting iteration")
+
+for(filename in todo){
+  print(filename)
+
+  #read the summary statistic in
+  ss <- read.table(paste0(filename), stringsAsFactors=F, header=T, sep='\t')
+  colnames(ss)[colnames(ss) == "other_allele"] <- "reference_allele"
+
+  #require both a column for major and minor allele, if only one exists refining is not possible
+  if("effect_allele" %in% colnames(ss) & "reference_allele" %in% colnames(ss)){
+    print("pass: allele cols present")
+
+    #limit SNPs to those with ACGT
+    ss$effect_allele <- toupper(ss$effect_allele)
+    ss$reference_allele <- toupper(ss$reference_allele)
+    ss <- ss[nchar(ss$effect_allele) == 1 & nchar(ss$reference_allele) == 1,]
+    ss <- ss[ss$effect_allele %in% c("A", "C", "G", "T") & ss$reference_allele %in% c("A", "C", "G", "T"),]
+
+    #remove ambigous SNPs
+    ss <- ss[!((ss$effect_allele == "A" & ss$reference_allele == "T") |
+               (ss$effect_allele == "T" & ss$reference_allele == "A") |
+               (ss$effect_allele == "G" & ss$reference_allele == "C") |
+               (ss$effect_allele == "C" & ss$reference_allele == "G")),]
+
+    #if rsID in the ss use it to match and sort to the UKBB
+    #if not then use the chromosome and position (I already checked that all scores' ref genomes match UKBB)
+    if("rsID" %in% colnames(ss)){
+      print("pass: rsid col present")
+      ss <- ss[!is.na(ss$rsID),]
+      ss$rsID <- tolower(ss$rsID)
+      ss <- ss[ss$rsID %in% impute$SNP,]
+      sub_impute <- impute[impute$SNP %in% ss$rsID,]
+      ss <- ss[order(ss$rsID)[rank(sub_impute$SNP)],]
+     
+
+      #Change the column names and then use the snp_match function from bigsnpr to flip alleles
+      colnames(sub_impute) <- c("loc", "rsid", "pos", "a0", "a1", "maf", "ax", "info", "chr", "superpos")
+      colnames(ss)[colnames(ss) == "effect_allele"] <- "a0"
+      colnames(ss)[colnames(ss) == "reference_allele"] <- "a1"
+      colnames(ss)[colnames(ss) == "rsID"] <- "rsid"
+      colnames(ss)[colnames(ss) == "effect_weight"] <- "beta"
+      ss$pos <- sub_impute$pos
+      ss$chr <- sub_impute$chr
+      ss <- snp_match(ss, sub_impute)
+
+      #write out the refined summary statistic
+      out_ss <- data.frame(chr = ss$chr, rsid = ss$rsid.ss, A1 = ss$a0, beta = ss$beta)
+      new_name <- strsplit(filename, ".", fixed = T)[[1]][1]
+
+      #iterating to make one file for each chromosome
+      print("writing out")
+      for(ii in 1:22){
+        if(ii %in% out_ss[,1]){
+        write.table(out_ss[out_ss[,1] == ii,], paste0("../pgs_sets/", new_name, ".", ii, ".ss"), col.names = T, row.names = F, sep = '\t', quote=F)
+        }
+      }
+      print("done\n")
+
+    }
+  }
+
+}
